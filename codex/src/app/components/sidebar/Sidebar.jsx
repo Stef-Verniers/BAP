dfcdba76e362ee735acbf41af35b1034f8b3f68c
Stef Verniers <91673619+Stef-Verniers@users.noreply.github.com>
2023-08-02 09:31:44 +0200

    Categorieën toegevoegd

--- codex/src/app/components/sidebar/Sidebar.jsx
+++ codex/src/app/components/sidebar/Sidebar.jsx
@@ -16,9 +16,20 @@ const Sidebar = () => {
             <li><a>Hoofdpagina</a></li>
             <li><a className='active'>Liederen</a></li>
             <ul className="subnav">
+                <li><a>Zoeken</a></li>
                 <li><a>Plechtige liederen</a></li>
                 <li><a>Vaderland</a></li>
                 <li><a>De Gilde Viert</a></li>
+                <li><a>Drinkliederen</a></li>
+                <li><a>Traditioneel</a></li>
+                <li><a>Die stem van Suid-Afrika</a></li>
+                <li><a>Die Burschenschaft</a></li>
+                <li><a>Over the Sea</a></li>
+                <li><a>Traditioneel</a></li>
+                <li><a>Au Quartier Latin</a></li>
+                <li><a>De wereld rond</a></li>
+                <li><a>Regionale Liederschat</a></li>
+                <li><a>Gallerij der Gentse Kringen en Clubs</a></li>
             </ul>
             <li><a>Generator</a></li>
         </ul>
