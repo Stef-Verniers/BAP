a272479b695298a8ef8b08b65f4510fb95173937
Stef Verniers <91673619+Stef-Verniers@users.noreply.github.com>
2023-08-02 19:22:23 +0200

    I've integrated chatgpt api

--- codex/server.js
+++ codex/server.js
@@ -5,4 +5,34 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
+
+const KEY = "Here is my special key that I've place here so I can develop";
+console.log(process.env.REACT_APP_API_KEY)
+console.log(import.meta.env.VITE_API_KEY)
+
+app.post('/completions', async (req, res) => {
+    const options = {
+        method: "post",
+        headers: {
+            "Authorization": `Bearer ${KEY}`,
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            model: "gpt-3.5-turbo",
+            messages: [{
+                role: 'user',
+                content: req.body.message
+            }],
+        })       
+    }
+
+    try {
+        const response = await fetch('https://api.openai.com/v1/chat/completions', options)
+        const data = await response.json()
+        res.send(data)
+    } catch (error) {
+        console.error(error)
+    }
+})
+
 app.listen(PORT, () => console.log('your server is running on port ' + PORT))
--- codex/src/app/components/views/generator/Generator.jsx
+++ codex/src/app/components/views/generator/Generator.jsx
@@ -1,6 +1,58 @@
 import './generator.css'
+import { useState } from 'react'
 
 const Generator = () => {
+
+    const [value, setValue] = useState(null)
+    const [message, setMessage] = useState(null)
+    const [request, setRequest] = useState('')
+    const [answer, setAnswer] = useState('')
+
+    // Request voor de API
+    const getMessages = async () => {
+        setRequest(value)
+        const options = {
+            method: "POST",
+            body: JSON.stringify(
+                {
+                    message: `Kan je mij een Vlaams cantusnummer genereren over ${value} dat rijmt`
+                }
+            ),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        }
+
+        try {
+           const response =  await fetch('http://localhost:8000/completions', options)
+           const data = await response.json()
+           setMessage(data.choices[0].message)
+           setAnswer(data.choices[0].message.content)
+           setValue('')
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+    // functie om tabs toe te voegen zodat het niet 1 lange tekst wordt.
+    function convertText(input) {
+        const sections = input.split(/\(([^)]+)\)/).filter(Boolean);
+
+        return (
+          <div>
+            {sections.map((section, index) => {
+              if (index % 2 === 1) {
+                return <h2 key={index} className='liedtekst'>{section.trim()}</h2>;
+              } else {
+                const sectionContent = section.trim().split(/,\s*/).join(',\n');
+                return <p key={index} className='aanduiding'>{sectionContent}</p>;
+              }
+            })}
+          </div>
+        );
+      }
+    
+
     return (
         <>
             <div className='generator' id='generator'>
@@ -15,12 +67,17 @@ const Generator = () => {
                     </p>
                     <section className='generator-section'>
                         <form className='generator-form'>
-                            <input type='text' placeholder='Een vos die in de overpoort loopt' />
-                            <button type='submit' className='button-1'>Genereren</button>
+                            <input type='text' placeholder='Een vos die in de overpoort loopt'  value={value} onChange={(e) => setValue(e.target.value) }/>
+                            <button type='submit' id='submit' onClick={getMessages} className='button-1'>Genereren</button>
                         </form>
                     </section>
-                    <section className='generator-answer' id='answer'>
-
+                    <section className='generator-answer' id='answer' style={{ display: request ? 'block' : 'none' }}>
+                        <div className="request">
+                            <strong><p>{request}</p></strong>
+                        </div>
+                        <div className="answer">
+                            {convertText(answer)}
+                        </div>
                     </section>
                 </div>
             </div>
