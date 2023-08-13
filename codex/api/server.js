aeb62b4927679f4e972cdfb9c9ce60f351adef2c
Stef Verniers <91673619+Stef-Verniers@users.noreply.github.com>
2023-08-13 16:18:46 +0200

    added functionality to server file

--- codex/api/server.js
+++ codex/api/server.js
@@ -4,12 +4,17 @@ import process from 'process'
 const app = express()
 app.use(express.json())
 app.use(cors())
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+ })
 
 app.get('/test', function(req, res){
     res.send("Hello from the root application URL");
 });
 
-app.post('/completions', async (req, res) => {
+app.post('/completions', cors(), async (req, res) => {
     const options = {
         method: "post",
         headers: {
